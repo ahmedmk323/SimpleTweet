862eae1d159544d8f8e301c2ee010c8f196a72ec
Ahmed Mohamed <ahmed.mk2981@gmail.com>
2022-10-11 01:08:03 -0400

    Second commit

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -1,13 +1,18 @@
 package com.codepath.apps.restclienttemplate
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
@@ -15,7 +20,7 @@ import okhttp3.Headers
 class ComposeActivity : AppCompatActivity() {
     lateinit var etCompose: EditText
     lateinit var btnTweet: Button
-
+    lateinit var tvCount: TextView
     lateinit var client: TwitterClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +28,29 @@ class ComposeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_compose)
         etCompose = findViewById(R.id.etTweetCompose)
         btnTweet = findViewById(R.id.btTweet)
+        tvCount = findViewById<TextView>(R.id.tvCount)
+
+        //Textwatcher to track the number of characters inputted
+        etCompose.addTextChangedListener(object : TextWatcher{
+            @SuppressLint("ResourceType")
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                tvCount.setTextColor(Color.GRAY)
+            }
+
+            @SuppressLint("ResourceType")
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.length!! == 280){
+                    tvCount.setTextColor(Color.RED)
+                }
+                tvCount.text = s?.length.toString()
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                btnTweet.isEnabled = !(s?.length!! > 280 || s?.length!! == 0)
+            }
+
+        })
 
         client = TwitterApplication.getRestClient(this)
 
@@ -38,8 +66,8 @@ class ComposeActivity : AppCompatActivity() {
                 Toast.makeText(this,"Empty tweets not allowed", Toast.LENGTH_SHORT).show()
             }
             // 2. Tweet must be under character count
-            else if (tweetContent.length > 140){
-                Toast.makeText(this,"Tweet is greater than 140 characters!!", Toast.LENGTH_SHORT).show()
+            else if (tweetContent.length > 280){
+                Toast.makeText(this,"Tweet is greater than 280 characters ${tweetContent.length}!!", Toast.LENGTH_SHORT).show()
             }else {
                 client.publishTweet(tweetContent, object : JsonHttpResponseHandler(){
                     override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
